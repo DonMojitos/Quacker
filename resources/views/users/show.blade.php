335f71d535659a987ecc2488808aff63b668208d
Ivan <ivanmorenolopez03@gmail.com>
2025-12-11 17:17:00 +0100

    pequeña errata con los names

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -3,7 +3,7 @@
 <body>
     <h1>Detalle de {{ $user->usuario }}</h1>
     <p><strong>ID:</strong> {{ $user->id }}</p>
-    <p><strong>Nombre Real:</strong> {{ $user->nombre }}</p>
+    <p><strong>Nombre Real:</strong> {{ $user->name }}</p>
     <p><strong>Email:</strong> {{ $user->email }}</p>
     <a href="/users">Volver al listado</a>
 </body>
